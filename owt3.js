d3b8a6fc8c78e14921daf1c95752a4e5efa02f4e
owt3 <baison0531@yahoo.co.jp>
2019-08-06 17:40:02 +0900

    電卓機能完成

--- owt3.js
+++ owt3.js
@@ -1,15 +1,15 @@
 "use strict";
 
 var state = ""; //現在の状態
-var totyuu; //演算子を押した後に入力した値
-var number = "0"; //初期の値
+var totyuu; //演算子を押した後に入力した値（途中結果）
+var number = ""; //初期の値
 var result; //計算結果の値
 
-function numberClick(obj) {
+function numberClick(obj) {　//数字をクリックした場合の処理
   if (number === "0") { //先頭に0が付かない為の実装
-    number = "";
+    number = ""; //先頭が0であればnumberをリセットしてしまう
   }
-  switch (state) { //
+  switch (state) { //計算の待機状態を計算結果を出す前、後で分けている
     case "plus_waiting":
       number = "";
       state = "plus";
@@ -41,26 +41,25 @@ function numberClick(obj) {
       totyuu = result;
       state = "multiplied";
       break;
-    case "divided_waiting":
+    case "divided_result_waiting":
       number = "";
       totyuu = result;
       state = "divided";
       break;
   }
-  if (number.length < 8) {
+  if (number.length < 8) { //入力の桁数を8桁に制限（小数点は1文字に含める）
     number += obj.value;
     display(number);
   }
 }
 
-function plusClick() {
+function plusClick() { //+を押した場合の処理
   switch (state) {
     case "plus_result_waiting": 
     case "minus_result_waiting": 
     case "multiplied_result_waiting": 
     case "divided_result_waiting": 
       totyuu = result;
-      state = "";
       break;
 
     case "plus": //a+b=と同じ挙動を+で代用
@@ -68,77 +67,105 @@ function plusClick() {
       display(result);
       state = "plus_result_waiting";
       break;
-
-    case "plus_waiting":
-      totyuu = result;
-      result = Number(totyuu) + Number(number);
-      display(result);
-      state = "plus_waiting";
-      break;
-
-    default:
-      totyuu = number;
-      state = "plus_waiting";
-      break;
-  }
-}
-
-function minusClick() {
-  switch (state) {
-    case "plus_result_waiting":
-    case "minus_result_waiting":
-    case "multiplied_result_waiting":
-    case "divided_result_waiting":
-      totyuu = result;
-      state = "minus_result_waiting";
-      break;
-
-      case "plus": //a+b=と同じ挙動を+で代用
-      result = Number(totyuu) + Number(number);
-      display(result);
-      state = "plus_result_waiting";
-      break;
       
-      case "minus": //a-b=と同じ挙動を-で代用
+    case "minus": //a-b=と同じ挙動を+で代用
       result = Number(totyuu) - Number(number);
       display(result);
       state = "minus_result_waiting";
       break;
 
-    case "minus_waiting":
-      totyuu = result;
-      result = Number(totyuu) - Number(number);
-      display(result);
-      state = "minus_waiting";
-      break;
-
-    default:
-      totyuu = number;
-      state = "minus_waiting";
-      break;
-  }
-}
-
-function multipliedClick() {
-  switch (state) {
-    case "plus_result_waiting":
-    case "minus_result_waiting":
-    case "multiplied_result_waiting":
-    case "divided_result_waiting":
-      totyuu = result;
-      break;
-
-    case "multiplied": //a*b=と同じ挙動を×で代用
+    case "multiplied": //a*b=と同じ挙動を+で代用
       result = Number(totyuu) * Number(number);
       display(result);
       state = "multiplied_result_waiting";
       break;
 
-    case "multiplied_waiting":
+    case "divided": //a/b=と同じ挙動を+で代用
+      result = Number(totyuu) / Number(number);
+      display(result);
+      state = "divided_result_waiting";
+      break;
+
+    default: //始めて押す時、計算結果を出す為にtotyuuとstateを定義する
+      totyuu = number;
+      state = "plus_waiting";
+      break;
+  }
+}
+
+function minusClick() { //-を押した場合の処理
+  switch (state) {
+    case "plus_result_waiting":
+    case "minus_result_waiting":
+    case "multiplied_result_waiting":
+    case "divided_result_waiting":
       totyuu = result;
+      state = "minus_result_waiting";
+      break;
+
+    case "plus":
       result = Number(totyuu) + Number(number);
       display(result);
-      state = "multiplied_waiting";
+      state = "plus_result_waiting";
+      break;
+      
+    case "minus":
+      result = Number(totyuu) - Number(number);
+      display(result);
+      state = "minus_result_waiting";
+      break;
+
+    case "multiplied":
+      result = Number(totyuu) * Number(number);
+      display(result);
+      state = "multiplied_result_waiting";
+      break;
+
+    case "divided":
+      result = Number(totyuu) / Number(number);
+      display(result);
+      state = "divided_result_waiting";
+      break;
+
+    default:
+      totyuu = number;
+      state = "minus_waiting";
+      break;
+  }
+}
+
+function multipliedClick() { //×を押した場合の処理
+  switch (state) {
+    case "plus_result_waiting":
+    case "minus_result_waiting":
+    case "multiplied_result_waiting":
+    case "divided_result_waiting":
+      totyuu = result;
+      state = "multiplied_result_waiting";
+      break;
+
+    case "plus":
+      result = Number(totyuu) + Number(number);
+      display(result);
+      state = "plus_result_waiting";
+      break;
+      
+    case "minus":
+      result = Number(totyuu) - Number(number);
+      display(result);
+      state = "minus_result_waiting";
+      break;
+
+    case "multiplied":
+      result = Number(totyuu) * Number(number);
+      display(result);
+      state = "multiplied_result_waiting";
+      break;
+
+    case "divided":
+      result = Number(totyuu) / Number(number);
+      display(result);
+      state = "divided_result_waiting";
       break;
 
     default:
@@ -146,30 +173,57 @@ function multipliedClick() {
       state = "multiplied_waiting";
       break;
   }
-
 }
 
-function dividedClick() {
-  totyuu = number;
+function dividedClick() { //÷を押した場合の処理
   switch (state) {
     case "plus_result_waiting": 
     case "minus_result_waiting": 
     case "multiplied_result_waiting": 
     case "divided_result_waiting": 
       totyuu = result;
+      state = "divided_result_waiting";
       break;
+  
+    case "plus":
+      result = Number(totyuu) + Number(number);
+      display(result);
+      state = "plus_result_waiting";
+      break;
+      
+    case "minus":
+      result = Number(totyuu) - Number(number);
+      display(result);
+      state = "minus_result_waiting";
+      break;
+
+    case "multiplied":
+      result = Number(totyuu) * Number(number);
+      display(result);
+      state = "multiplied_result_waiting";
+      break;
+
+    case "divided":
+      result = Number(totyuu) / Number(number);
+      display(result);
+      state = "divided_result_waiting";
+      break;
+
+    default:
+        totyuu = number;
+        state = "divided_waiting";
+        break;
   }
-  state = "divided_waiting";
 }
 
-function dotClick() {
+function dotClick() { //.を押した場合、数列内に.が入ってない場合代入できるようにしている
   if (number.indexOf(".") < 0) {
     number += ".";
     display(number);
   }
 }
 
-function equalClick() {
+function equalClick() { //=を押した場合の処理、stateによって計算する式を変えている
   switch (state) {
     case "plus":
       result = Number(totyuu) + Number(number);
@@ -210,12 +264,12 @@ function equalClick() {
   }
 }
 
-function clearClick() {
+function clearClick() { //ACを押した場合の処理、
   state = "";
   totyuu = undefined;
-  display(number = "0");
+  display(number = "0"); //現在の状態、途中結果、初期の値をリセットしている
 }
 
-function display(msg) {
+function display(msg) { //画面上に表示する為の関数
   displayArea.innerHTML = msg;
 }
